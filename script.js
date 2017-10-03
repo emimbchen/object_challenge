310aefd22cd409da4c743da5276545c53e89ee52
Emi Chen <emimbchen@gmail.com>
2017-10-03 18:28:11 -0500

    completed Pizza for the People

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 var locations = []; // this array will hold your objects
 
 // object constructor
-
+function State(stateIn, revenueIn, locationCountIn){
+  this.state = stateIn;
+  this.revenue = revenueIn;
+  this.locationCount = locationCountIn;
+  this.averageRevenue = function(){
+    return (this.revenue/this.locationCount).toFixed(2);
+  };
+}
 // object instances
+var ilLocation = new State('IL', 5000, 12);
+var mnLocation = new State('MN', 300, 3);
+var nvLocation = new State('NV', 25000, 1);
 
 // push object instances to locations array
+locations.push(ilLocation);
+locations.push(mnLocation);
+locations.push(nvLocation);
 
 // DO NOT MODIFY THIS CODE
 $(document).ready(function () {
